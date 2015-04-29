5f9b59868194a5f29cec145e01b5b2cf1c435eef
alumne <alumne@maqueta>
2015-04-29 19:02:57 +0200

    Commit 1A

--- index.php
+++ index.php
@@ -1,11 +1,2 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <h1>Exercici ONE</h1>
-        <p>Lorem ipsum</p>
-    </body>
-</html>
+<h1>Exercici ONE</h1>
+<p>Lorem ipsum</p>
